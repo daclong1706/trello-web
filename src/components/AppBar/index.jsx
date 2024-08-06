0af95314b6b01accfd3bcd0683fa9d2aec3926aa
Nguyen Dac Long <daclong.k17@gmail.com>
2024-08-06 23:10:49 +0700

    Code header

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,16 +1,151 @@
+import { useState } from 'react'
 import Box from '@mui/material/Box'
 import ModeSelect from '~/components/ModeSelect'
+import AppsIcon from '@mui/icons-material/Apps'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import AddBoxIcon from '@mui/icons-material/AddBox'
+import SearchIcon from '@mui/icons-material/Search'
+import CloseIcon from '@mui/icons-material/Close'
+
+import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
+import SvgIcon from '@mui/material/SvgIcon'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Badge from '@mui/material/Badge'
+import IconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
+import InputAdornment from '@mui/material/InputAdornment'
+
+import Workspaces from './Menus/Workspaces'
+import Recent from './Menus/Recent'
+import Starred from './Menus/Starred'
+import Templates from './Menus/Templates'
+import Profiles from './Menus/Profiles'
+import theme from '~/theme'
+
 
 function AppBar() {
+    const [searchValue, setSearchValue] = useState('')
     return (
         <Box sx={{
-            backgroundColor: 'primary.light',
             width: '100%',
             height: (theme) => theme.trello.appBarHeight,
             display: 'flex',
-            alignItems: 'center'
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            gap: 2,
+            paddingX: 2,
+            overflowX: 'auto',
+            bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#17153B' : '#836fff')
         }}>
-            <ModeSelect />
+            {/* <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar> */}
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                <AppsIcon sx={{ color: 'white' }} />
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+                    <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color: 'white' }} />
+                    <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'white' }}>Trello</Typography>
+                </Box>
+                <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 1 }}>
+                    <Workspaces />
+                    <Recent />
+                    <Starred />
+                    <Templates />
+                    <Button
+                        sx={{
+                            color: 'white',
+                            border: 'none',
+                            '&:hover': { border: 'none' }
+                        }}
+                        variant="outlined"
+                        startIcon={<AddBoxIcon />}
+                    >
+                        Create
+                    </Button>
+                </Box>
+
+
+            </Box>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                <TextField
+                    id="outlined-search"
+                    label="Search..."
+                    type="text"
+                    size='small'
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon sx={{ color: 'white' }} />
+                            </InputAdornment>
+                        ),
+                        endAdornment: (
+                            <CloseIcon
+                                fontSize='small'
+                                sx={{
+                                    color: searchValue ? 'white' : 'transparent',
+                                    cursor: 'pointer'
+                                }}
+                                onClick={() => setSearchValue('')}
+                            />
+                        )
+                    }}
+                    sx={{
+                        minWidth: 120,
+                        maxWidth: 170,
+                        '& label': { color: 'white' },
+                        '& input': { color: 'white' },
+                        '& label.Mui-focused': { color: 'white' },
+                        '& .MuiOutlinedInput-root': {
+                            '& fieldset': {
+                                borderColor: 'white'
+                            },
+                            '&:hover fieldset': {
+                                borderColor: 'white'
+                            },
+                            '&.Mui-focused fieldset': {
+                                borderColor: 'white'
+                            }
+                        }
+                    }}
+                />
+                <ModeSelect />
+                <Tooltip title="Notification">
+                    <IconButton>
+                        <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }}>
+                            <NotificationsNoneIcon sx={{ color: 'white' }} />
+                        </Badge>
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="Help">
+                    <IconButton>
+                        <HelpOutlineIcon sx={{ cursor: 'pointer', color: 'white' }} />
+                    </IconButton>
+                </Tooltip>
+
+                <Profiles />
+
+            </Box>
+
         </Box>
     )
 }
--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,76 @@
 /* eslint-disable indent */
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-import { deepOrange, orange, teal, cyan } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
     trello: {
-        appBarHeight: '48px',
-        boardBarHeight: '58px'
+        appBarHeight: '58px',
+        boardBarHeight: '48px'
     },
     colorSchemes: {
-        light: {
-            palette: {
-                primary: teal,
-                secondary: deepOrange
+        // light: {
+        //     palette: {
+        //         primary: teal,
+        //         secondary: deepOrange
+        //     }
+        // },
+        // dark: {
+        //     palette: {
+        //         primary: cyan,
+        //         secondary: orange
+        //     }
+        // }
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    textTransform: 'none',
+                    borderWidth: '0.5px',
+                    '&:hover': {
+                        borderWidth: '1px'
+                    }
+                }
             }
         },
-        dark: {
-            palette: {
-                primary: cyan,
-                secondary: orange
+        MuiInputLabel: {
+            styleOverrides: {
+                root: {
+                    fontSize: '0.875rem'
+                }
+            }
+        },
+        MuiOutlinedInput: {
+            styleOverrides: {
+                root: {
+                    fontSize: '0.875rem',
+                    '& fieldset': {
+                        borderWidth: '0.5px !important'
+                    },
+                    '&:hover fieldset': {
+                        borderWidth: '1px !important'
+                    },
+                    '&.Mui-focused fieldset': {
+                        borderWidth: '1px !important'
+                    }
+                }
+            }
+        },
+        MuiCssBaseline: {
+            styleOverrides: {
+                body: {
+                    '*::-webkit-scrollbar': {
+                        width: '8px',
+                        height: '8px'
+                    },
+                    '*::-webkit-scrollbar-thumb': {
+                        backgroundColor: '#bdc3c7',
+                        borderRadius: '8px'
+                    },
+                    '*::-webkit-scrollbar-thumb:hover': {
+                        backgroundColor: 'white'
+                    }
+                }
             }
         }
     }
